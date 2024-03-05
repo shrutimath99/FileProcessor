771bd1b1f09646052ddbcd109630988079f69df7
shrutimath99 <shrutimath99@gmail.com>
2024-03-05 20:33:07 +0530

    Removed print statement

--- fileprocessor.py
+++ fileprocessor.py
@@ -13,7 +13,6 @@ newdf = df1._append(df2)
 
 # Dropping duplicates from dataframe
 result = newdf.drop_duplicates()
-print("Length of Unique data", len(result))
 
 
 # Adding Gross Salary  column to result CSV file which is sum of basic_salary and allowances
@@ -23,16 +22,9 @@ result["Gross Salary"] = result["basic_salary"]+result["allowances"]
 result.to_csv("Result.csv",index=False)
 
 # Calculating mean or Average of basic salary
-# We can use mean function for average gross salary but mean function takes all rows of csv including column 
-# names which slightly gives wrong result result  Hence I have used a sum function to calculate the result
-#  and divide it by number of rows in dataframe
-average_gross_salary=result["Gross Salary"].mean()
-# Sum = result["Gross Salary"].sum()
-# print("Rows are ",result.shape[0])
-# average_gross_salary =  round(Sum/(result.shape[0]-1),5)
-# print(average_gross_salary)
 
-print("Length of Salaries",len(result["Gross Salary"]))
+average_gross_salary=result["Gross Salary"].mean()
+
 
 #Dropping duplicates to calculate 2nd highest salary
  
@@ -41,10 +33,11 @@ unique_gross_salaries = result['Gross Salary'].drop_duplicates()
 # Selecting 2nd Highest Gross salary value
 second_highest_salary = result['Gross Salary'].sort_values(ascending=False).iloc[1]
 
-print("second_highest_salary",second_highest_salary,type(second_highest_salary))
 
 
 with open("Result.csv","a") as f:
     f.write(f"Second Highest Salary={second_highest_salary},average salary={average_gross_salary},,,,,,")
+
+print("Result.csv file generated Successfully.")
 
 
